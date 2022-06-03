cc58fb6d00e08bc4c05e5dc45b0238d00e3dd08e
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-06-03 17:26:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,42 +2,17 @@ import time
 
 import tweepy
 
-consumer_key = 'KvANtIhF89JvRMxeyJFfKN2Av'
-consumer_secret = 'CjXjIVevlu96LeVTyXkvlFuIweMAVBXAtFa2ymMKLpSYLqe5i3'
-access_token = '1172074378353229824-Q5PNl31DHV7AC8HrChvFFtiFwztjNh'
-access_token_secret = 'U2sJ9LGzbZPVV0EbBaYRtn5LI3T4bn7WFT6gvZ6eGDIXg'
+consumer_key = 'use your consumer key'
+consumer_secret = 'use your consumer secret'
+access_token = 'use your access token'
+access_token_secret = 'use your access token secret'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-# api.update_status('Hey Folks...!')
 
-# tweets = api.mentions_timeline()
-
-
-# print(tweets[4].text)
-
-# -----------------PRINT ALL MENTION TWEET (WITHOUT ANY CONDITION)------------------
-
-# for tweet in tweets:
-#     print(str(tweet.id) + ' - ' + tweet.text)
-
-# -------------WITH CONDITION-------------
-# for tweet in tweets:
-#     if '#aktweet' in tweet.text:
-#         print("New Tweet Found!")
-#         print(str(tweet.id) + ' - ' + tweet.text)
-
-
-# -----------REPLY TO THE MENTIONED TWEET (USING THE FILE 'last_seen.txt')------------
 FILE_NAME = 'last_seen.txt'
-
-
-# for tweet in tweets:
-#     if '#aktweet' in tweet.text:
-#         print("New Tweet Found!")
-#         print(str(tweet.id) + ' - ' + tweet.text)
 
 def read_last_seen(FILE_NAME):
     file_read = open(FILE_NAME, 'r')
@@ -67,20 +42,3 @@ def reply():
 while True:
     reply()
     time.sleep(15)
-
-# ---------------USE TO DISPLAY THE ID OF THE TWEET (THE ID WHICH ARE MENTION IN THE FILE i.e., last_seen.txt)---------
-# id = read_last_seen(FILE_NAME)
-# print(id)
-
-# tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode='extended')
-# for tweet in tweets:
-#     if '#aktweet' in tweet.full_text.lower:
-#         print("New Tweet Found!")
-#         print(str(tweet.id) + ' - ' + tweet.full_text)
-#
-#         store_last_seen(FILE_NAME, tweet.id)
-#
-#         # print(str(tweet.id) + ' - ' + tweet.full_text)
-
-
-
