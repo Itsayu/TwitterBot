b943be48542ab9658c46f58c8975ae373f39bd5f
Ayush Kumar <94097145+Itsayu@users.noreply.github.com>
2022-06-03 17:23:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
 import tweepy
 
-consumer_key = 'KvANtIhF89JvRMxeyJFfKN2Av'
-consumer_secret = 'CjXjIVevlu96LeVTyXkvlFuIweMAVBXAtFa2ymMKLpSYLqe5i3'
-access_token = '1172074378353229824-Q5PNl31DHV7AC8HrChvFFtiFwztjNh'
-access_token_secret = 'U2sJ9LGzbZPVV0EbBaYRtn5LI3T4bn7WFT6gvZ6eGDIXg'
+consumer_key = 'use your consumer key'
+consumer_secret = 'use your consumer secret'
+access_token = 'use your access token'
+access_token_secret = 'use your access token secret'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 api.update_status('Hey Folks...!')
-
-# tweets = api.mentions_timeline()
-# print(tweets[4].text)
-
-# -----------------PRINT ALL MENTION TWEET (WITHOUT ANY CONDITION)------------------
-
-# for tweet in tweets:
-#     print(str(tweet.id) + ' - ' + tweet.text)
-
-# -------------WITH CONDITION-------------
-# for tweet in tweets:
-#     if '#aktweet' in tweet.text:
-#         print("New Tweet Found!")
-#         print(str(tweet.id) + ' - ' + tweet.text)
-
-
